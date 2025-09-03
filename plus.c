0ee8070451565d152f5186be145f2208812d3e92
Divyank <divyanksinghoffi@gmail.com>
2025-09-04 02:01:29 +0530

    Add files via upload

---
+++ plus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int x,y,s;
+    printf("Enter the first number:");
+    scanf("%d", &x);
+    printf("Enter the second number:");
+    scanf("%d", &y);
+    s = x+y;
+    printf("Sum is :%d\n", s);
+    
+    return 0;
+}
