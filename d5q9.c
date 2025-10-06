730e5e6ca72140b7c79b06621c69320863b591e7
Divyank <divyanksinghoffi@gmail.com>
2025-10-06 14:51:52 +0530

    Add files via upload
    day 5 question 9 simple intrest

---
+++ d5q9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+// simple intrest
+int main(){
+    float p = 34.1;
+    int r = 8;
+    int t = 5;
+    printf("The value of simple intrest id %f",(p*t*r)/100);
+    return 0 ;
+
+}
